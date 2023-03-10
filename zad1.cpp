66ac26ae3fe69ddadf5853f255aa0f16e78261d5
Blindk1ng <118027070+Blindk1ng@users.noreply.github.com>
2023-03-10 04:37:41 +0300

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int *a = new int(50);
+	cout << "adress \t *a\n";
+	cout << a << "\t" << *a << endl;
+	(*a)++;
+	cout << a << "\t" << *a << endl;
+	*(a++);
+	cout << a << "\t" << *a << endl;
+	*a++;
+	return 0;
+} 
